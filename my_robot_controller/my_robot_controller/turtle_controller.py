b1b9fc5c4607c2489f2766965320c42ded2b4988
wukongxzero <pavankushal15729@gmail.com>
2023-03-24 19:31:40 +0530

    closed loop control

--- my_robot_controller/my_robot_controller/turtle_controller.py
+++ my_robot_controller/my_robot_controller/turtle_controller.py
@@ -1,26 +1,38 @@
 #!usr/bin/env python3
+
+#closed loop control
+
 import rclpy
 from rclpy.node import Node
 from turtlesim.msg import Pose
-def main(args = None):
-    rclpy.init(args=args)
-    node = TurtleControllerNode()
-    rclpy.spin(node)
-    rclpy.shutdown()
+from geometry_msgs.msg import Twist
+
 
 class TurtleControllerNode(Node):
 
     def __init__(self):
         super().__init__("turtle_controller")
-        self.cmd_vel_publisher_ = self.create_publisher(Twist, "/turtle/cmd_vel", 10)
+        self.cmd_vel_publisher_ = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
         self.pose_subscriber_ = self.create_subscription(Pose, "/turtle1/pose",self.pose_callback, 10)
         self.get_logger().info("turtle controller has been activated")
     
     def pose_callback(self, pose: Pose):
+
         cmd = Twist()
-        cmd.linear.x = 5.0
-        cmd.angular.z = 0.0
+
+        if pose.x > 9.0 or pose.x < 2.0 or pose.y > 9.0 or pose.y < 2.0:
+            cmd.linear.x = 1.0
+            cmd.angular.z = 0.9
+        else:
+            cmd.linear.x = 5.0
+            cmd.angular.z = 0.0
         self.cmd_vel_publisher_.publish(cmd)
+
+def main(args = None):
+    rclpy.init(args=args)
+    node = TurtleControllerNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
 
 
 
