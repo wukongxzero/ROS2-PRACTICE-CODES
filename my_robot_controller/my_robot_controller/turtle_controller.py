243f3606480afd55252cd44c516fc9f81824fb1b
wukongxzero <pavankushal15729@gmail.com>
2023-03-26 14:57:59 +0530

    service node red to green transition

--- my_robot_controller/my_robot_controller/turtle_controller.py
+++ my_robot_controller/my_robot_controller/turtle_controller.py
@@ -6,12 +6,15 @@ import rclpy
 from rclpy.node import Node
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
+from turtlesim.srv import SetPen
+from functools import partial
 
 
 class TurtleControllerNode(Node):
 
     def __init__(self):
         super().__init__("turtle_controller")
+        self.previous_x_ = 0
         self.cmd_vel_publisher_ = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
         self.pose_subscriber_ = self.create_subscription(Pose, "/turtle1/pose",self.pose_callback, 10)
         self.get_logger().info("turtle controller has been activated")
@@ -27,6 +30,42 @@ class TurtleControllerNode(Node):
             cmd.linear.x = 5.0
             cmd.angular.z = 0.0
         self.cmd_vel_publisher_.publish(cmd)
+
+        if pose.x > 5.5 and self.previous_x_ <= 5.5:
+            self.previous_x_ = pose.x
+            self.get_logger().info("set color to red")
+            self.call_set_pen_service(255,0,0,3,0)
+        elif pose.x<5.5 and self.previous_x_ >= 5.5:
+            self.previous_x_ = pose.x
+            self.get_logger().info("set color to green")
+            self.call_set_pen_service(0, 255, 0, 3, 0)
+
+
+
+
+
+        
+    def call_set_pen_service( self, r, g, b, width, off):
+        client = self.create_client(SetPen, "/turtle1/set_pen")
+        while not client.wait_for_service(1.0):
+            self.get_logger().warn("waiting for service..") 
+        
+        request = SetPen.Request()
+        request.r = r
+        request.g = g
+        request.b = b
+        request.width = width
+        request.off = off
+
+        future = client.call_async(request)
+        future.add_done_callback(partial(self.callback_set_pen))  
+
+    def callback_set_pen(self, future):
+        try: 
+            response = future.result()
+        except Exception as e:
+            self.get_logger().error("service call failed: %r" % (e,))   
+
 
 def main(args = None):
     rclpy.init(args=args)
